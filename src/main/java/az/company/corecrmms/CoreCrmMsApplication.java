6ae69fe885e487dc14449fc068c7deb044140db2
Mahammad <mahammad.code@gmail.com>
2025-05-18 14:42:06 +0400

    Initialize application

---
+++ src/main/java/az/company/corecrmms/CoreCrmMsApplication.java
@@ -0,0 +1,13 @@
+package az.company.corecrmms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreCrmMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreCrmMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/company/corecrmms/CoreCrmMsApplicationTests.java
@@ -0,0 +1,12 @@
+package az.company.corecrmms;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreCrmMsApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
